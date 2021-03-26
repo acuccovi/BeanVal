16dbb211f6b346a55a4f7d21babc717e909aa2e7
Alessio Cuccovillo <alessio.cuccovillo@gmail.com>
2021-03-26 17:22:07 +0100

    Refactor dei test

--- src/test/java/cuccovillo/alessio/beanval/BeanValApplicationTests.java
+++ src/test/java/cuccovillo/alessio/beanval/BeanValApplicationTests.java
@@ -4,30 +4,32 @@ import cuccovillo.alessio.beanval.model.Faclim;
 import cuccovillo.alessio.beanval.model.GenericField;
 import cuccovillo.alessio.beanval.model.Proter;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
-import java.util.Set;
+import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Log4j2
 class BeanValApplicationTests {
+    private static Validator validator;
 
-    @Test
-    void contextLoads() {
+    @BeforeAll
+    static void contextLoads() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
     }
 
     @Test
     void validData() {
         Proter p = new Proter();
         p.setValue("Ciao");
-        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
-        Set<ConstraintViolation<Proter>> violations = validator.validate(p);
+        Collection<ConstraintViolation<GenericField>> violations = validate(p);
         assertTrue(violations.isEmpty());
     }
 
@@ -35,8 +37,7 @@ class BeanValApplicationTests {
     void invalidData() {
         Proter p = new Proter();
         p.setValue("Ciao!");
-        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
-        Set<ConstraintViolation<GenericField>> violations = validator.validate(p);
+        Collection<ConstraintViolation<GenericField>> violations = validate(p);
         assertFalse(violations.isEmpty());
     }
 
@@ -44,8 +45,7 @@ class BeanValApplicationTests {
     void invalidDataWithDefaultMessage() {
         Proter p = new Proter();
         p.setValue("Ciao!");
-        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
-        Set<ConstraintViolation<GenericField>> violations = validator.validate(p);
+        Collection<ConstraintViolation<GenericField>> violations = validate(p);
         for (ConstraintViolation<GenericField> violation : violations) {
             assertEquals("Value not valid", violation.getMessage());
             log.info("Default message: [{}]", violation.getMessage());
@@ -56,13 +56,16 @@ class BeanValApplicationTests {
     void invalidDataWithCustomMessage() {
         Faclim f = new Faclim();
         f.setValue("Ciao!");
-        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
-        Set<ConstraintViolation<GenericField>> violations = validator.validate(f);
+        Collection<ConstraintViolation<GenericField>> violations = validate(f);
         assertFalse(violations.isEmpty());
         for (ConstraintViolation<GenericField> violation : violations) {
             assertEquals("Cambia questo valore", violation.getMessage());
             log.info("Custom message: [{}]", violation.getMessage());
         }
+    }
+
+    private Collection<ConstraintViolation<GenericField>> validate(GenericField field) {
+        return validator.validate(field);
     }
 
 
